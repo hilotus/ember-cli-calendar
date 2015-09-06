ca55e5980a809f3f5d017e212dddb3b5ed74b76f
wluo <wei.luo@careerbuilder.com>
2015-09-06 17:00:05 +0800

    update calendar model...

--- addon/components/em-calendar.js
+++ addon/components/em-calendar.js
@@ -7,15 +7,12 @@ export default Ember.Component.extend({
   className: 'default',
 
   cssClass: Ember.computed('className', {
-    get: function() {
+    get: function () {
       return "em-" + this.className + "-cal";
     },
-    set: function(k, v) {
-      if (v === void 0) {
-        this.set('className', 'default');
-      } else {
-        this.set('className', v);
-      }
+
+    set: function (k, v) {
+      this.set('className', v);
       return "em-" + this.className + "-cal";
     }
   }),
@@ -24,61 +21,74 @@ export default Ember.Component.extend({
   weekOptions: ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'],
   monthOptions: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
   monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-  model: [],
 
-  init: function() {
-    var model, today;
-    this._super();
-    model = this.get('model');
-    if (!Ember.isArray(this.model) || this.model.length !== 3) {
-      throw new Ember.Error("Calendar's model must be an array of length 3.");
-    }
-    this.set('year', model[0]);
-    this.set('month', model[1]);
-    this.set('date', model[2]);
-    today = new Date();
-    this.set('currentYear', today.getFullYear());
-    this.set('currentMonth', today.getMonth() + 1);
-    return this.set('currentDate', today.getDate());
-  },
-
-  monthName: Ember.computed('month', function() {
-    return this.monthNames[this.month - 1];
+  /*
+    [year, month, date]
+    [2014, 10, 10]
+  */
+  today: Ember.computed(function () {
+    var t = new Date();
+    return [t.getFullYear(), t.getMonth() + 1, t.getDate()];
   }),
 
-  weekDates: Ember.computed('year', 'month', function() {
+  selected: [],
+
+  model: Ember.computed('selected', {
+    get: function () {
+      return this.get('selected');
+    },
+
+    set: function (key, value) {
+      value = value || this.get('today');
+      this.set('selected', value);
+      return value;
+    }
+  }),
+
+  year: Ember.computed('model', function () {
+    return this.get('model')[0];
+  }),
+
+  month: Ember.computed('model', function () {
+    return this.get('model')[1];
+  }),
+
+  date: Ember.computed('model', function () {
+    return this.get('model')[2];
+  }),
+
+  monthName: Ember.computed('month', function () {
+    var monthNames = this.get('monthNames');
+    return monthNames[this.get('month') - 1];
+  }),
+
+  weekDates: Ember.computed('year', 'month', function () {
     var dates;
-    dates = this.get("__cache." + this.year + "-" + this.month);
+    dates = this.get("__cache." + this.get('year') + "-" + this.get('month'));
     if (dates) {
       return dates;
     }
-    dates = generateDates(this.year, this.month);
-    this.set("__cache." + this.year + "-" + this.month, dates);
+    dates = generateDates(this.get('year'), this.get('month'));
+    this.set("__cache." + this.get('year') + "-" + this.get('month'), dates);
     return dates;
   }),
 
-  selectedDay: Ember.computed('year', 'month', 'date', function() {
-    return [this.year, this.month, this.date];
-  }),
-
-  today: Ember.computed('currentYear', 'currentMonth', 'currentDate', function() {
-    return [this.currentYear, this.currentMonth, this.currentDate];
-  }),
-
-  cnCalendar: Ember.computed('year', 'month', 'date', function() {
-    return Calendar.generate(this.year, this.month, this.date);
+  cnCalendar: Ember.computed('year', 'month', 'date', function () {
+    return Calendar.generate(this.get('year'), this.get('month'), this.get('date'));
   }),
 
   actions: {
     selectMonth: function(month) {
-      return this.set('month', month + 1);
+      var _sel = this.get('selected');
+      this.set('selected', [_sel[0], window.parseInt(month) + 1, _sel[2]]);
     },
 
     selectDate: function(date) {
-      this.set('year', date.get('gregorianYear'));
-      this.set('month', date.get('gregorianMonth'));
-      this.set('date', date.get('gregorianDate'));
-      return this.set('model', [this.year, this.month, this.date]);
+      var _year = window.parseInt(date.get('gregorianYear')),
+        _month = window.parseInt(date.get('gregorianMonth')),
+        _date = window.parseInt(date.get('gregorianDate'));
+
+      this.set('selected', [_year, _month, _date]);
     }
   }
 });
--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  model: [2012, 5, 21],
-  cssClass: 'helveti'
+  model: null
 });
